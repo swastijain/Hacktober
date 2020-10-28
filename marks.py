1a98aea61bc67cc690bdcc2dd3ab8b31e5d911d0
Bhavya Jain <bhavyajain020105@gmail.com>
2020-10-28 22:41:20 +0530

    remove # end

--- marks.py
+++ marks.py
@@ -1,12 +1,13 @@
 list1 = []
-n= int(input( "how many students marks you want to enter" :))
+print("how many students marks you want to enter" )
+n= int(input( ))
 for i in range(0,n):
- print("Enter marks of student",(i+1),":")
- marks = int(input())
- list1.append(marks)
-  total = 0
+  print("Enter marks of student",(i+1),":")
+  marks = int(input())
+  list1.append(marks)
+   total = 0
  for marks in list1:
    total = total + marks
- average = total / n
-print("Average marks of",n,"students is:",average)
-# end   
+ Average = total / n
+print("Average marks of",n,"students is:",Average)
+# end  
