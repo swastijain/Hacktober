8a6c940724f5553a32d8870890116eaa95ba42bd
unknown <swasti61204@gmail.com>
2020-10-31 13:00:23 +0530

    Added a random file

---
+++ flag.py
@@ -0,0 +1,12 @@
+T=int(input())
+flag=0
+a=[]
+for i in range(0,T):
+    no=int(input())
+    #
+    for j in no:
+        if j==4:
+            flag+=1
+        print(flag)
+    
+            
--- valid_triangle.py
+++
@@ -1,9 +0,0 @@
-#https://www.codechef.com/problems/FLOW013
-T=int(input())
-for i in range (0,T):
-    A,B,C=input().split()
-    if A+B+C==180:
-        print("YES")
-    else:
-        print("NO")
-        
