85cccfd8137ae109483eee3920171d389370178e
unknown <swasti61204@gmail.com>
2020-10-31 13:07:33 +0530

    added another file rectangle

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+T=int(input())
+l=[]
+for i in range(0,T):
+    a,b,c,d=input().split()
+    l.append(a,b,c,d)
+    print(l)
+
+
+
+
+    '''
+    if a=b and a=c and a=d:
+        print("NO")
+    elif b=c and b=d:
+        print("NO")
+    elif c=d:
+    '''
